71a4922831112fe6ae376f7cefbd00f2a1190709
Piyush Kumar <piyushkumar.verma@sapient.com>
2020-05-25 02:59:00 +0530

    fix component render error

--- src/templates/Portfolio/Portfolio.js
+++ src/templates/Portfolio/Portfolio.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import Stock from "../../components/Organism/portfolio/stock";
+import Stock from "../../components/Organism/portfolio/stock.vue";
 import { mapGetters } from "vuex";
 export default Vue.extend({
   components: {
